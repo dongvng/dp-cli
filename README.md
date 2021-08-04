56e3aa9edf9b58a1d48ceab0178dba5aedf512ca
Dong Nguyen <dongvn666@gmail.com>
2021-08-04 15:42:54 +0700

    rlz: init cli

---
+++ README.md
@@ -0,0 +1,1 @@
+## Playground in building a node cli
---
+++ bin/dp-cli.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const yargs = require('yargs');
+
+const options = yargs
+  .usage('Usage: -n <name>')
+  .option('n', {
+    alias: 'name',
+    describe: 'Your name',
+    type: 'string',
+    demandOption: true,
+  })
+  .help().argv;
+
+console.log(options);
+const greeting = `Hello, ${options.name}!`;
+
+console.log(greeting);
